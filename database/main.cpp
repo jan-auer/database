650d5a92dcb2cbebf47ca540f601549da39c15f7
Jan Michael Auer <jan.auer@me.com>
2014-04-08 15:39:38 +0200

    Add a sample XCode project.

---
+++ database/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  database
+//
+//  Created by Jan Michael Auer on 08/04/14.
+//  Copyright (c) 2014 TUM. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
