2b10011deab1561e481a2d3505a4ac638de6e108
Jan Michael Auer <jan.auer@me.com>
2014-04-11 18:59:49 +0200

    Add a basic FileUtils class.

---
+++ database/utils/FileUtils.cpp
@@ -0,0 +1,93 @@
+//
+//  Utils.cpp
+//  database
+//
+//  Created by Jan Michael Auer on 11/04/14.
+//  Copyright (c) 2014 LightningSQL. All rights reserved.
+//
+
+#include <fcntl.h>
+#include <unistd.h>
+#include <iostream>
+#include "FileUtils.h"
+
+using namespace std;
+
+#if defined(__APPLE__)
+int posix_fallocate(int fd, off_t offset, off_t len);
+#endif
+
+namespace lsql {
+
+	int FileUtils::openRead(const char* location) {
+		int fd = open(location, O_RDONLY);
+		if (fd < 0) {
+			cerr << "Cannot open file '" << location << "' for read access: "
+			     << strerror(errno) << endl;
+		}
+		return fd;
+	}
+	
+	int FileUtils::openWrite(const char* location) {
+		int fd = open(location, O_CREAT|O_TRUNC|O_WRONLY, S_IRUSR|S_IWUSR);
+		if (fd < 0) {
+			cerr << "Cannot open file '" << location << "' for write access: "
+			     << strerror(errno) << endl;
+		}
+		return fd;
+	}
+	
+	bool FileUtils::close(int fd) {
+		return ::close(fd);
+	}
+	
+	template<typename T>
+	bool FileUtils::allocate(int fd, off_t elementCount) {
+		off_t fileSize = elementCount * sizeof(T);
+		int ret = posix_fallocate(fd, 0, fileSize);
+		
+		if (ret == 0) {
+			return true;
+		} else {
+			cerr << "Warning: Could not allocate file space: " << strerror(ret) << endl;
+			return false;
+		}
+	}
+	
+	template<typename T>
+	vector<T> FileUtils::readAll() {
+		return vector<T>();
+	}
+	
+	template<typename T>
+	bool FileUtils::writeAll(int fd, vector<T> &data) {
+		const T* rawData = data.data();
+		size_t rawSize = sizeof(T) * data.size();
+		
+		ssize_t writtenSize = write(fd, rawData, rawSize);
+		if (writtenSize == rawSize) {
+			return true;
+		} else {
+			cerr << "Cannot write to file: " << strerror(errno) << endl;
+			return false;
+		}
+	}
+	
+}
+
+# if defined(__APPLE__)
+
+// dirty workaround to have sth similar to posix_fallocate on BSD Systems
+// taken from: https://trac.transmissionbt.com/browser/branches/1.5x/libtransmission/fdlimit.c?rev=8203#L170
+int posix_fallocate(int fd, off_t offset, off_t len) {
+	fstore_t fst;
+	fst.fst_flags = F_ALLOCATECONTIG;
+	fst.fst_posmode = F_PEOFPOSMODE;
+	fst.fst_offset = offset;
+	fst.fst_length = len;
+	fst.fst_bytesalloc = 0;
+	
+	return fcntl( fd, F_PREALLOCATE, &fst );
+}
+
+#endif
