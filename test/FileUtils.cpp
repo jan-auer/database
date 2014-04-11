3d3d5161e3a6d989e41cf33c594cfaf9829056e2
Jan Michael Auer <jan.auer@me.com>
2014-04-11 19:02:19 +0200

    Add test stubs for FileUtils.

---
+++ test/FileUtils.cpp
@@ -0,0 +1,15 @@
+//
+//  FileUtils.cpp
+//  database
+//
+//  Created by Jan Michael Auer on 11/04/14.
+//  Copyright (c) 2014 LightningSQL. All rights reserved.
+//
+
+#include <stdio.h>
+#include "gtest.h"
+#include "FileUtils.h"
+
+using namespace lsql;
+
+#warning Unit Tests not implemented!
