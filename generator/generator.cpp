f4944b4e823fa2b4b27748fa9f9a02abe0fbf3c7
Jan Michael Auer <jan.auer@me.com>
2014-04-11 19:02:36 +0200

    Refactor the generator.

--- generator/generator.cpp
+++ generator/generator.cpp
@@ -1,73 +1,66 @@
 #include <iostream>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <stdlib.h>
-#include <string.h>
-#include <errno.h>
-#include <stdint.h>
-#include <unistd.h>
+#include "FileUtils.h"
 
-class RandomLong
-{
-		/// The state
-		uint64_t state;
-		
+using namespace std;
+using namespace lsql;
+
+const int BUFFER_SIZE = 1000;
+
+class RandomLong {
+	
+private:
+	uint64_t state;
+	
 public:
-		/// Constructor
-		explicit RandomLong(uint64_t seed=88172645463325252ull) : state(seed) {}
-		
-		/// Get the next value
-		uint64_t next() { state^=(state<<13); state^=(state>>7); return (state^=(state<<17)); }
-		
-		
+	explicit RandomLong(uint64_t seed=88172645463325252ull) : state(seed) {}
+	
+	uint64_t next() {
+		state^=(state<<13);
+		state^=(state>>7);
+		return (state^=(state<<17));
+	}
+	
 };
 
-
-# if defined(__APPLE__)
-
-// dirty workaround to have sth similar to posix_fallocate on BSD Systems
-// taken from: https://trac.transmissionbt.com/browser/branches/1.5x/libtransmission/fdlimit.c?rev=8203#L170
-
-int posix_fallocate(int fd, off_t offset, off_t len)	 {
-		
-		fstore_t fst;
-		fst.fst_flags = F_ALLOCATECONTIG;
-		fst.fst_posmode = F_PEOFPOSMODE;
-		fst.fst_offset = offset;
-		fst.fst_length = len;
-		fst.fst_bytesalloc = 0;
-		
-		return fcntl( fd, F_PREALLOCATE, &fst );
-		
-}
-
-#endif
-
 int main(int argc, char* argv[]) {
-		if (argc < 3) {
-				std::cerr << "usage: " << argv[0] << " <file name> <number of elements>" << std::endl;
+	RandomLong rand;
+	int fd;
+	unsigned int n;
+
+	if (argc < 3) {
+		cerr << "usage: " << argv[0] << " <file name> <number of elements>" << endl;
+		return -1;
+	}
+	
+	n = atoi(argv[2]);
+	if (n == 0) {
+		cerr << "invalid length: " << argv[2] << endl;
+		return -1;
+	}
+	
+	fd = FileUtils::openWrite(argv[1]);
+	if (fd < 0)
+		return -1;
+	
+	if (!FileUtils::allocate<uint64_t>(fd, n))
+		return -1;
+	
+	vector<uint64_t> data(BUFFER_SIZE);
+	for (int i = 0; i < n; i++) {
+		data.push_back(rand.next());
+		if (i % BUFFER_SIZE == 0) {
+			if (!FileUtils::writeAll(fd, data))
 				return -1;
+			
+			data.clear();
 		}
-		RandomLong rand;
-		unsigned n = atoi(argv[2]);
-		if (n==0) {
-				std::cerr << "invalid length: " << argv[2] << std::endl;
-				return -1;
-		}
-		int fd, ret;
-		if ((fd = open(argv[1], O_CREAT|O_TRUNC|O_WRONLY, S_IRUSR|S_IWUSR)) < 0) {
-				std::cerr << "cannot open file '" << argv[1] << "': " << strerror(errno) << std::endl;
-				return -1;
-		}
-		if ((ret = posix_fallocate(fd, 0, n*sizeof(uint64_t))) != 0)
-				std::cerr << "warning: could not allocate file space: " << strerror(ret) << std::endl;
-		for (unsigned i=0; i<n; ++i) {
-				uint64_t x = rand.next();
-				if (write(fd, &x, sizeof(uint64_t)) < 0) {
-						std::cout << "error writing to " << argv[1] << ": " << strerror(errno) << std::endl;
-				}
-		}
-		close(fd);
-		return 0;
+	}
+	
+	if (!FileUtils::writeAll(fd, data))
+		return -1;
+	
+	if (!FileUtils::close(fd))
+		return -1;
+	
+	return 0;
 }
