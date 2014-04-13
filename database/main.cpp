4dececf1b317fd0c036dc5bc950c54555e9182c3
Jan Michael Auer <jan.auer@me.com>
2014-04-13 19:36:29 +0200

    Adapt the main test scenario.

--- database/main.cpp
+++ database/main.cpp
@@ -15,10 +15,14 @@ using namespace lsql;
 
 int main(int argc, const char * argv[])
 {
-	int numbers = FileUtils::openRead("numbers");
-	Sorting::externalSort(numbers, 10, 0, 6 * sizeof(uint64_t));
+	int input = FileUtils::openRead("numbers");
+	int output = FileUtils::openWrite("sorted");
 	
-	std::cout << "Hello, World!\n";
+	Sorting::externalSort(input, 20, output, 10 * sizeof(uint64_t));
+	
+	FileUtils::close(output);
+	FileUtils::close(input);
+	
     return 0;
 }
 
