bcbf4bc81239761c99c1f4ce312f10daeb9f9c34
Jan Michael Auer <jan.auer@me.com>
2014-04-16 23:40:07 +0200

    Add a tests stub for File.

---
+++ test/FileTest.cpp
@@ -0,0 +1,13 @@
+//
+//  FileTest.cpp
+//  database
+//
+//  Created by Jan Michael Auer on 16/04/14.
+//  Copyright (c) 2014 LightningSQL. All rights reserved.
+//
+
+#include <stdio.h>
+#include "gtest.h"
+#include "File.h"
+
+#warning Tests not implemented.
