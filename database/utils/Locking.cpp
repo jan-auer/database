853c4bf905c7d08a66ad2455b44264af204cdf96
Jan Michael Auer <jan.auer@me.com>
2014-04-19 02:22:21 +0200

    Add utilities for concurrency control using pthread.
     - Type definition lsql::Lock.
     - lsql::lock for acquiring a lock.
     - lsql::unlock for releasing a lock.

---
+++ database/utils/Locking.cpp
@@ -0,0 +1,32 @@
+//
+//  Locking.cpp
+//  database
+//
+//  Created by Jan Michael Auer on 19/04/14.
+//  Copyright (c) 2014 LightningSQL. All rights reserved.
+//
+
+#include "Locking.h"
+
+#define __lock(target, exclusive, section) {\
+          Lock* __l = &(target).lock; \
+          lsql::lock(__l, exclusive); \
+          { section } \
+          lsql::unlock(__l); \
+        }
+
+namespace lsql {
+	
+	int lock(Lock* lock, bool exclusive) {
+		if (exclusive) {
+			return pthread_rwlock_wrlock(lock);
+		} else {
+			return pthread_rwlock_rdlock(lock);
+		}
+	}
+	
+	int unlock(Lock* lock) {
+		return pthread_rwlock_unlock(lock);
+	}
+	
+}
