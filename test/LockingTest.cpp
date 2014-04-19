dc7685ad5813a9006e2ce4b4f230fc5a7d33131f
Jan Michael Auer <jan.auer@me.com>
2014-04-19 02:23:54 +0200

    Add tests and helpers for concurrency.
    NOTE: These tests currently fail due to an unknown reason. This
    has to be resolved as soon as possible!

---
+++ test/LockingTest.cpp
@@ -0,0 +1,60 @@
+//
+//  LockingTest.cpp
+//  database
+//
+//  Created by Jan Michael Auer on 19/04/14.
+//  Copyright (c) 2014 LightningSQL. All rights reserved.
+//
+
+#include <iostream>
+#include "gtest/gtest.h"
+#include "helpers/ThreadHelpers.cpp"
+#include "Locking.h"
+
+using namespace lsql;
+
+namespace lsql {
+namespace test {
+
+	TEST(LockingTest, LocksShared) {
+		Lock l = PTHREAD_RWLOCK_INITIALIZER;
+		
+		lock(&l, false);
+		ssize_t ret = testThreadded(&testSharedLock, (void*) &l);
+		
+		pthread_rwlock_destroy(&l);
+		EXPECT_EQ(0, ret);
+	}
+
+	TEST(LockingTest, LocksExclusive) {
+		Lock l = PTHREAD_RWLOCK_INITIALIZER;
+		
+		lock(&l, true);
+		ssize_t ret = testThreadded(&testExclusiveLock, (void*) &l);
+		
+		pthread_rwlock_destroy(&l);
+		EXPECT_EQ(0, ret);
+	}
+
+	TEST(LockingTest, UnlocksShared) {
+		Lock l = PTHREAD_RWLOCK_INITIALIZER;
+		pthread_rwlock_rdlock(&l);
+		
+		unlock(&l);
+		int ret = pthread_rwlock_unlock(&l);
+		
+		EXPECT_EQ(EPERM, ret);
+	}
+
+	TEST(LockingTest, UnlocksExclusive) {
+		Lock l = PTHREAD_RWLOCK_INITIALIZER;
+		pthread_rwlock_wrlock(&l);
+		
+		unlock(&l);
+		int ret = pthread_rwlock_unlock(&l);
+		
+		EXPECT_EQ(EPERM, ret);
+	}
+
+}
+}
---
+++ test/helpers/ThreadHelpers.cpp
@@ -0,0 +1,45 @@
+//
+//  ThreadHelpers.cpp
+//  database
+//
+//  Created by Jan Michael Auer on 19/04/14.
+//  Copyright (c) 2014 LightningSQL. All rights reserved.
+//
+
+#ifndef __unit_test__ThreadHelpers__
+#define __unit_test__ThreadHelpers__
+
+#include <pthread.h>
+
+namespace lsql {
+namespace test {
+
+	void* testSharedLock(void* arg) {
+		if (pthread_rwlock_tryrdlock((pthread_rwlock_t*) arg) != 0)
+			return (void*) 1;
+		if (pthread_rwlock_trywrlock((pthread_rwlock_t*) arg) == 0)
+			return (void*) 2;
+		
+		return (void*) 0;
+	}
+
+	void* testExclusiveLock(void* arg) {
+		return (void*) (pthread_rwlock_tryrdlock((pthread_rwlock_t*) arg) == 0);
+	}
+
+	ssize_t testThreadded(void* (*callback)(void *), void* arg) {
+		pthread_t thread;
+		void* ret;
+		
+		if (pthread_create(&thread, nullptr, callback, arg) != 0)
+			return -1;
+		if (pthread_join(thread, &ret) != 0)
+			return -1;
+		
+		return (ssize_t)ret;
+	}
+
+}
+}
+
+#endif /* defined(__unit_test__ThreadHelpers__) */
