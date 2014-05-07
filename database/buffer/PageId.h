e8850bdaa143572ff43037ca436afcf7808d525c
Jan Michael Auer <jan.auer@me.com>
2014-05-07 20:50:46 +0200

    Move PageId to the right namespace and update tests.

--- database/buffer/PageId.h
+++ database/buffer/PageId.h
@@ -2,38 +2,50 @@
 //  PageId.h
 //  database
 //
-//  Created by Jan Michael Auer on 21/04/14.
+//  Created by Jan Michael Auer on 04/05/14.
 //  Copyright (c) 2014 LightningSQL. All rights reserved.
 //
 
 #ifndef database_PageId_h
 #define database_PageId_h
 
-/**
- * A page id containing the unique segment identifier and the page identifier
- * which is unique within the segment.
- *
- * The memory layout allows the PageId to be casted from and to uint64_t. 
- * Additionally, an implicit constructor from uint64_t allows quick conversions.
- */
-struct PageId {
+namespace lsql {
 
-	uint64_t id;
+	/**
+	 * A page id containing the unique segment identifier and the page identifier
+	 * which is unique within the segment.
+	 *
+	 * The memory layout allows the PageId to be casted from and to uint64_t.
+	 * Additionally, an implicit constructor from uint64_t allows quick conversions.
+	 */
+	struct PageId {
 
-	PageId(uint64_t id) : id(id) {}
+		uint64_t id;
 
-	PageId(uint32_t segment = 0, uint32_t page = 0) {
-		id = uint64_t(segment) << 32 | page;
+		PageId(uint64_t id) : id(id) {}
+
+		PageId(uint32_t segment, uint32_t page) {
+			id = uint64_t(segment) << 32 | page;
+		}
+
+		const uint32_t segment() const {
+			return uint32_t(id >> 32);
+		}
+
+		const uint32_t page() const {
+			return uint32_t(id);
+		}
+
+	};
+
+	bool operator==(const PageId& a, const PageId& b) {
+		return a.id == b.id;
 	}
 
-	const uint32_t segment() const {
-		return uint32_t(id >> 32);
+	bool operator!=(const PageId& a, const PageId& b) {
+		return !(a == b);
 	}
 
-	const uint32_t page() const {
-		return uint32_t(id);
-	}
-
-};
+}
 
 #endif
