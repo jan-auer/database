e046a8921d102fd0483f0e2c5e16099f62bc14b1
Jan Michael Auer <jan.auer@me.com>
2014-05-07 21:10:06 +0200

    Implement the BufferManager correctly.

--- database/buffer/BufferManager.cpp
+++ database/buffer/BufferManager.cpp
@@ -2,78 +2,58 @@
 //  BufferManager.cpp
 //  database
 //
-//  Created by Jan Michael Auer on 17/04/14.
+//  Created by Jan Michael Auer on 04/05/14.
 //  Copyright (c) 2014 LightningSQL. All rights reserved.
 //
 
+#include <utility>
 #include <cmath>
-#include "BufferManager.h"
 
-using namespace std;
+#include "utils/Logger.h"
+#include "utils/File.h"
+#include "BufferManager.h"
 
 namespace lsql {
 
-	BufferManager::BufferManager(const string& fileName, uint64_t size)
-	: file(fileName, true), maxPages(size) {
-		assert(maxPages > 0);
+	BufferManager::BufferManager(uint64_t size) : freePages(size) {
+		assert(freePages > 0);
 
+		queueThreshold = BUFFER_MANAGER_QUEUE_THRESHOLD * size;
 		// TODO: Come up with something better than log2(float)
-		slotCount = uint64_t(1) << int(ceil(log2(maxPages)));
-
-		slots = new BufferSlot[slotCount];
-		slotLocks = new Lock[slotCount];
+		slotCount = uint64_t(1) << int(ceil(log2(size)));
+		pageTable = new Slot[slotCount];
 	}
 
 	BufferManager::~BufferManager() {
-		for (int i = 0; i < slotCount; i++) {
-			for (BufferEntry& entry : slots[i]) {
-				// TODO: Flush page contents to disc
-				delete entry.second;
-			}
-		}
+		for (; slotCount > 0; --slotCount)
+			pageTable[slotCount - 1].cleanup();
 
-		delete[] slots;
-		delete[] slotLocks;
+		delete[] pageTable;
 	}
 
 	BufferFrame& BufferManager::fixPage(const PageId& id, bool exclusive) {
-		BufferFrame* frame;
-		BufferSlot& slot = slots[hash(id)];
+		mutex.lock();
 
-		// Get a slot lock to prevent it from being changed
-		Lock slotLock = slotLocks[hash(id)];
-		slotLock.lock(false);
+		BufferFrame* frame;
+		Slot& slot = getSlot(id);
 
 		// Search for the frame
-		frame = acquirePage(slot, id, exclusive);
+		frame = acquirePage(slot, id);
 		if (frame != nullptr) {
-			slotLock.unlock();
-			return *frame;
-		}
-
-		// Upgrade the slot lock to an exclusive lock
-		slotLock.unlock();
-		slotLock.lock(true);
-
-		// Research for the frame, in case someone was faster than us
-		frame = acquirePage(slot, id, exclusive);
-		if (frame != nullptr) {
-			slotLock.unlock();
+			frame->lock(exclusive);
+			mutex.unlock();
 			return *frame;
 		}
 
 		// Create and insert a new frame
 		frame = allocatePage(slot, id);
-		frame->lock(true);
-		slotLock.unlock();
+		mutex.unlock();
 
 		// Load data into the new frame
-		file.read(frame->getData(), BufferFrame::SIZE);
+		frame->load();
 
 		// Downgrade the lock, if only a shared lock is desired
 		if (!exclusive) {
-			// Downgrading the lock requires to release it first. It is very
-			// unlikely, that the page gets paged out so quickly.
 			frame->unlock();
 			frame->lock(false);
 		}
@@ -93,24 +73,88 @@ namespace lsql {
 		return id.page() & (slotCount - 1);
 	}
 
-	BufferFrame* BufferManager::acquirePage(BufferSlot& slot, const PageId& id, bool exclusive) {
-		for (BufferEntry& entry : slot) {
-			if (id == entry.first) {
-				entry.second->lock(exclusive);
-				return entry.second;
-			}
-		}
-
-		return nullptr;
+	BufferManager::Slot& BufferManager::getSlot(const PageId& id) const {
+		return pageTable[hash(id)];
 	}
 
-	BufferFrame* BufferManager::allocatePage(BufferSlot& slot, const PageId& id) {
-		// TODO: Page out old pages, if necessary.
+	BufferFrame* BufferManager::acquirePage(Slot& slot, const PageId& id) {
+		BufferFrame* frame = slot.getFirst();
+		while (frame != nullptr && id != frame->getId())
+			frame = frame->tableNext;
 
-		BufferFrame* frame = new BufferFrame(id);
-		slot.emplace_back(id, frame);
+		if (frame != nullptr)
+			registerPageAccess(frame);
+
+		return frame;
+	}
+
+	void BufferManager::registerPageAccess(BufferFrame* frame) {
+		if (frame->queue == QUEUE_AM) {
+			queueAm.bringFront(frame);
+		} else {
+			queueA1.remove(frame);
+			frame->queue = QUEUE_AM;
+			queueAm.prepend(frame);
+		}
+	}
+
+	BufferFrame* BufferManager::allocatePage(Slot& slot, const PageId& id) {
+		BufferFrame* frame;
+
+		if (freePages > 0) {
+			frame = new BufferFrame(id);
+			freePages--;
+		} else {
+			BufferFrame* unused = findUnusedPage();
+			unused->save();
+
+			frame = new BufferFrame(id, std::move(*unused));
+
+			unused->unlock();
+			delete unused;
+		}
+
+		frame->lock(true);
+		slot.prepend(frame);
+
+		frame->queue = QUEUE_A1;
+		queueA1.prepend(frame);
+
+		return frame;
+	}
+
+	BufferFrame* BufferManager::findUnusedPage() {
+		BufferFrame* unused;
+
+		if (queueA1.getSize() > queueThreshold) {
+			unused = getLastUnusedPage(queueA1);
+			if (unused == nullptr)
+				unused = getLastUnusedPage(queueAm);
+		} else {
+			unused = getLastUnusedPage(queueAm);
+			if (unused == nullptr)
+				unused = getLastUnusedPage(queueA1);
+		}
+
+		if (unused == nullptr) {
+			std::cerr << "ERROR: Out of memory." << std::endl;
+			exit(EXIT_CODE_NO_MORE_MEMORY);
+		}
+
+		return unused;
+	}
+
+	BufferFrame* BufferManager::getLastUnusedPage(Queue& queue) {
+		BufferFrame* frame = queue.getLast();
+		while (frame != nullptr && !frame->tryLock(true))
+			frame = frame->queuePrev;
+
+		if (frame == nullptr)
+			return nullptr;
+
+		getSlot(frame->getId()).remove(frame);
+		queue.remove(frame);
 		return frame;
 	}
 
 }
-
--- database/buffer/BufferManager.h
+++ database/buffer/BufferManager.h
@@ -2,19 +2,22 @@
 //  BufferManager.h
 //  database
 //
-//  Created by Jan Michael Auer on 17/04/14.
+//  Created by Jan Michael Auer on 04/05/14.
 //  Copyright (c) 2014 LightningSQL. All rights reserved.
 //
 
 #ifndef __database__BufferManager__
 #define __database__BufferManager__
 
-#include <cstdint>
-#include <utility>
-#include <vector>
+#include <atomic>
 
-#include "utils/Lock.h"
+#include "utils/ConcurrentList.h"
+#include "utils/Mutex.h"
+#include "PageId.h"
 #include "BufferFrame.h"
+
+#define BUFFER_MANAGER_QUEUE_THRESHOLD 0.5
+#define EXIT_CODE_NO_MORE_MEMORY 42
 
 namespace lsql {
 
@@ -25,41 +28,64 @@ namespace lsql {
 	 */
 	class BufferManager {
 
-		typedef std::pair<PageId, BufferFrame*> BufferEntry;
-		typedef std::vector<BufferEntry> BufferSlot;
+		/**
+		 * Implementation of the ConcurrentList AccessPolicy for Queues.
+		 */
+		struct QueuePolicy {
+			BufferFrame*& next(BufferFrame* frame) const { return frame->queueNext; }
+			BufferFrame*& prev(BufferFrame* frame) const { return frame->queuePrev; }
+		};
 
-		Lock* slotLocks;
-		BufferSlot* slots;
+		/**
+		 * Implementation of the ConcurrentList AccessPolicy for the page table.
+		 */
+		struct TablePolicy {
+			BufferFrame*& next(BufferFrame* frame) const { return frame->tableNext; }
+			BufferFrame*& prev(BufferFrame* frame) const { return frame->tablePrev; }
+		};
+		
+		/**
+		 * Type definition for threadsafe queues of buffer frames.
+		 */
+		typedef ConcurrentList<BufferFrame, QueuePolicy> Queue;
+
+		/**
+		 * Type definition for threadsafe lists of buffer frames in page tables.
+		 */
+		typedef ConcurrentList<BufferFrame, TablePolicy> Slot;
+
+		Mutex mutex;
+		uint64_t freePages;
+		uint64_t queueThreshold;
 		uint64_t slotCount;
 
-		File<void> file;
-		uint64_t maxPages;
-		
+		Slot* pageTable;
+		Queue queueA1;
+		Queue queueAm;
+
 	public:
-		
+
 		/**
 		 * Creates a new buffer manager instance which operates on the
 		 * specified file.
 		 *
-		 * @param fileName Fully qualified path to the file containing
-		 *                 the frames. The file will be opened with r+.
-		 * @param size     The maximum number of frames in memory.
+		 * @param size The maximum number of frames in memory.
 		 */
-		BufferManager(const std::string& fileName, uint64_t size);
-		
+		BufferManager(uint64_t size);
+
 		/**
 		 * Destroys this buffer manager instance and writes all dirty
 		 * frames to disc.
 		 */
 		~BufferManager();
-		
+
 		/**
 		 * Retrieves the specified frame. If the frame is not in memory,
 		 * it will be loaded from disc. This might force old unfixed
 		 * frames to get flushed to disc and removed from memory.
 		 *
 		 * If there is no space left for new frames, this method might
-		 * fail to return a frame.
+		 * fail to return a frame and will exit with an error message.
 		 *
 		 * @param id        An identifier for the page to load.
 		 * @param exclusive Whether or not this frame is exclusive to the
@@ -69,7 +95,7 @@ namespace lsql {
 		 *         for more details.
 		 */
 		BufferFrame& fixPage(const PageId& id, bool exclusive);
-		
+
 		/**
 		 * Releases the given frame. Thus, the frame can be paged out,
 		 * if necessary. Previous calls to @c BufferFrame::fixPage with
@@ -80,11 +106,11 @@ namespace lsql {
 	private:
 
 		/**
-		 * Creates a hash value for the specified page using an internal 
-		 * hashing algorithm. 
-		 * 
-		 * The hash is guaranteed not to exceed @c slotCount, if slotCount is 
-		 * a power of two. Otherwise, the hashed value is undefined. 
+		 * Creates a hash value for the specified page using an internal
+		 * hashing algorithm.
+		 *
+		 * The hash is guaranteed not to exceed @c slotCount, if slotCount is
+		 * a power of two. Otherwise, the hashed value is undefined.
 		 *
 		 * @param id The page identifier to obtain the hash value from.
 		 * @return The hashed value of @c id.
@@ -92,29 +118,75 @@ namespace lsql {
 		uint64_t hash(const PageId& id) const;
 
 		/**
+		 * Returns a reference to the appropriate slot in the page table
+		 * determined by the internal hash function.
+		 *
+		 * @param id The page identifier to obtain the hash value from.
+		 * @return A reference to the slot.
+		 */
+		Slot& getSlot(const PageId& id) const;
+
+		/**
 		 * Resolves a buffer frame within the given slot.
 		 *
-		 * @param slot      A reference to the slot to search in.
-		 * @param id        The id of the page.
-		 * @param exclusive Whether or not the frame is exclusive to the caller 
-		 *                  of this method.
+		 * @param slot A reference to the slot to search in.
+		 * @param id   The id of the page.
 		 *
 		 * @return A pointer to the page frame, if found; otherwise @c nullptr.
 		 */
-		BufferFrame* acquirePage(BufferSlot& slot, const PageId& id, bool exclusive);
+		BufferFrame* acquirePage(Slot& slot, const PageId& id);
 
 		/**
-		 * Creates a new buffer frame and allocates space for the page contents. 
-		 * 
+		 * Registers an access to the specified page for the page replacement
+		 * algorithm to prevent immediate page outs.
+		 */
+		void registerPageAccess(BufferFrame* frame);
+
+		/**
+		 * Creates a new buffer frame and allocates space for the page contents.
+		 * If there is no more memory for new pages, an old page is written to 
+		 * disk and removed from memory first
+		 *
 		 * @param slot A reference to the slot which will contain the frame.
 		 * @param id   The id of the page.
 		 *
 		 * @return A pointer to the new page frame.
 		 */
-		BufferFrame* allocatePage(BufferSlot& slot, const PageId& id);
-		
+		BufferFrame* allocatePage(Slot& slot, const PageId& id);
+
+		/**
+		 * Finds an unused page according to the page replacement algorithm.
+		 * This method might fail, if there are no unused pages in memory.
+		 * In this case an error message is printed and the function
+		 * exits with code EXIT_CODE_NO_MORE_MEMORY.
+		 * A BufferFrame is returned so that the memory can be reused and no new
+		 * malloc calls needs to be done.
+		 *
+		 * @return BufferFrame* an unused buffer frame, removed from the queue
+		 */
+		BufferFrame* findUnusedPage();
+
+		/**
+		 * Finds the last unused page in the given queue and removes it from 
+		 * memory.
+		 *
+		 * To find the last unused page, this method scans all BufferFrames 
+		 * in this queue in reverse order. To prevent deadlocks, first the table 
+		 * slot of the respective frame is locked (blocking). Then, an exclusive
+		 * lock of the frame is attempted: 
+		 *  - If this lock is successful, the page is removed from all lists and 
+		 *    written to disc. 
+		 *  - Otherwise, the frame is discarded and all locks are freed.
+		 *
+		 * This method might fail to remove a page, if there are no unused
+		 * pages in the given queue.
+		 *
+		 * @return True, if a page was freed; otherwise false;
+		 */
+		BufferFrame* getLastUnusedPage(Queue& queue);
+
 	};
-	
+
 }
 
 #include "BufferManager.cpp"
