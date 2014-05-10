5099a8a954a382144e266ece6ef6bf83ef5f5ff5
Felix Jankowski <felix@jankowski.me>
2014-05-10 22:30:27 +0200

    Add schema declaration

---
+++ database/schema/parser/Schema.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <vector>
+#include <string>
+#include "Types.h"
+
+struct Schema {
+   struct Relation {
+      struct Attribute {
+         std::string name;
+         Types::Tag type;
+         unsigned len;
+         bool notNull;
+         Attribute() : len(~0), notNull(true) {}
+      };
+      std::string name;
+      std::vector<Schema::Relation::Attribute> attributes;
+      std::vector<unsigned> primaryKey;
+      Relation(const std::string& name) : name(name) {}
+   };
+   std::vector<Schema::Relation> relations;
+   std::string toString() const;
+};
