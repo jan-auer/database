d9b3b38d337ba697df47264f07093ff1d3b01a6d
Felix Jankowski <felix@jankowski.me>
2014-05-24 16:02:18 +0200

    Starting to understand class templates. I am such an idiot -.-

--- database/index/btree/BTree-impl.h
+++ database/index/btree/BTree-impl.h
@@ -1,34 +1,89 @@
 //
-//  BTree.cpp
+//  BTree.h
 //  database
 //
 //  Created by Felix Jankowski on 19.05.14.
 //  Copyright (c) 2014 LightningSQL. All rights reserved.
 //
 
+#pragma once
 #include "BTree.h"
 
+#include <iostream>
+#include "buffer/BufferManager.h"
 #include "BTreeNode.h"
-
+#include <vector>
 
 namespace lsql {
 
+	/**
+	 * This is a B+-Tree datastructure for indexing data
+	 *
+	 * @param Keys The datatype of the indexed values
+	 * @param Comperator A class providing the < comperator for each data type
+	 */
 	template<class Key, class Comperator>
-	BTree::BTree(BufferManager& bufferManager, uint16_t segmentId, uint32_t pageCount = 0)
-	: Segment(bufferManager, segmentId, pageCount) {}
+	class BTree : protected Segment {
+
+	private:
+		uint64_t size;
+
+	public:
+
+		/**
+		 * Creates a wrapper for segments containing a B+-Tree.
+		 *
+		 * @param bufferManager The buffer manager instance.
+		 * @param segmentId     The segment identifier.
+		 * @param pageCount     The number of pages in this segment.
+		 */
+		BTree(BufferManager& bufferManager, uint16_t segmentId, uint32_t pageCount = 0)
+		: Segment(bufferManager, segmentId, pageCount) {}
 
 
-	template<class Key, class Comperator>
-	bool BTree::insert(const Key& key, const TID& tid) {
-		BufferFrame& frame = findFreeFrame(record.getSize());
+		template<class Key, class Comperator>
+		bool BTree::insert(const Key& key, const TID& tid) {
+			BufferFrame& frame = findFreeFrame(record.getSize());
 
-		BTreeNode bn(this, frame);
-		TID id = bn.createSlot();
-		bn.insert(id, record);
+			BTreeNode bn(this, frame);
+			TID id = bn.createSlot();
+			bn.insert(id, record);
 
-		unfixPage(frame, true);
-		return id;
-	}
-	
+			unfixPage(frame, true);
+			return id;
+		}
+
+
+		/**
+		 *
+		 */
+		bool insert(const Key& key, const TID& tid);
+
+
+		/**
+		 *
+		 */
+		bool erase(const Key& key);
+
+		/**
+		 *
+		 */
+		TID lookup(const Key& key) const ;
+
+		/**
+		 *
+		 */
+		std::vector<TID> lookupRange(const Key& key) const;
+
+
+		/**
+		 * Getter for the size
+		 */
+		uint64_t getSize();
+
+	};
 
 }
+
+#include "BTree-impl.h"
+
--- database/index/btree/BTreeNode-impl.h
+++ database/index/btree/BTreeNode-impl.h
@@ -1,35 +1,84 @@
 //
-//  BTreeNode.cpp
+//  BTreeNode.h
 //  database
 //
 //  Created by Felix Jankowski on 19.05.14.
 //  Copyright (c) 2014 LightningSQL. All rights reserved.
 //
 
-#include "BTreeNode.h"
+#pragma once
+#include <iostream>
+
+#include "common/IDs.h"
+#include "segment/Segment.h"
 
 namespace lsql {
 
+
 	template<class Key, class Comperator>
-	BTreeNode::BTreeNode(BufferFrame& frame, NodeType type)
-	: pid(frame.getId()) {
+	class BTreeNode {
+
+		enum NodeType {
+			Inner, Leaf
+		};
+
+		struct Header {
+			NodeType type;
+			uint64_t LSN = 0;
+			uint32_t count;
+		};
+
+		struct InnerHeader : Header	{
+			const NodeType type = Inner;
+		};
+
+		struct LeafHeader : Header {
+			const NodeType type = Leaf;
+			uint64_t LSN = 0;
+			uint32_t count;
+			PID next;
+		};
 
 
-		int32_t headerSize = sizeof(Header) + (header->count + 1) * sizeof(Slot);
-		return int32_t(BufferFrame::SIZE) - header->usedSpace - headerSize;
+		PID pid;
+		char* data;
+		Header* header;
 
-		header = static_cast<Header*>(frame.getData());
+	public:
 
-		if (type == Inner) {
-			header = static_cast<InnerHeader*>(header);
-			data = reinterpret_cast<char*>(data + sizeof(InnerHeader));
-		} else if (type == Leaf) {
-			header = static_cast<LeafHeader*>(header);
-			data = reinterpret_cast<char*>(data + sizeof(LeafHeader));
+		/**
+		 * Creates a new node in the B+-Tree.
+		 *
+		 * @param frame   The corresponding buffer frame containing page data.
+		 */
+		BTreeNode::BTreeNode(BufferFrame& frame, NodeType type)
+		: pid(frame.getId()) {
+
+
+			int32_t headerSize = sizeof(Header) + (header->count + 1) * sizeof(Slot);
+			return int32_t(BufferFrame::SIZE) - header->usedSpace - headerSize;
+
+			header = static_cast<Header*>(frame.getData());
+
+			if (type == Inner) {
+				header = static_cast<InnerHeader*>(header);
+				data = reinterpret_cast<char*>(data + sizeof(InnerHeader));
+			} else if (type == Leaf) {
+				header = static_cast<LeafHeader*>(header);
+				data = reinterpret_cast<char*>(data + sizeof(LeafHeader));
+			}
+			
 		}
 
-	}
 
+		TID lookup(Key& key) const;
 
+		bool insert(const Key& key, const TID& tid);
+
+		bool remove(Key& key);
+
+		std::pair<TID, TID> splitNode();
+
+	};
 
 }
