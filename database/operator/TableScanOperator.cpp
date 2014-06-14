3edbd567b5cbc9edc73366a829da19bc48ed0f34
Jan Michael Auer <jan.auer@me.com>
2014-06-14 20:12:43 +0200

    Implement the table scan operator.

---
+++ database/operator/TableScanOperator.cpp
@@ -0,0 +1,93 @@
+//
+//  TableScanOperator.cpp
+//  database
+//
+//  Created by Jan Michael Auer on 14/06/14.
+//  Copyright (c) 2014 LightningSQL. All rights reserved.
+//
+
+#include "TableScanOperator.h"
+
+namespace lsql {
+
+	TableScanOperator::TableScanOperator(Relation& relation)
+	:segment(relation), page(nullptr), isOpen(false) {
+	}
+
+	void TableScanOperator::open() {
+		assert(!isOpen);
+		
+		pages = segment.begin();
+		isOpen = true;
+	}
+
+	bool TableScanOperator::next() {
+		assert(isOpen);
+
+		while (pages != segment.end()) {
+			if (page == nullptr) {
+				page = &(*pages);
+				tuples = page->begin();
+			}
+
+			if (tuples != page->end()) {
+				output = buildRow(*tuples);
+				++tuples;
+				return true;
+			}
+
+			++pages;
+		}
+
+		return false;
+	}
+
+	Row TableScanOperator::getOutput() const {
+		assert(isOpen);
+		return output;
+	}
+
+	void TableScanOperator::rewind() {
+		assert(isOpen);
+
+		close();
+		open();
+	}
+
+	void TableScanOperator::close() {
+		assert(isOpen);
+
+		page = nullptr;
+		pages = segment.end();
+		isOpen = false;
+	}
+
+	Row TableScanOperator::buildRow(const Record& record) const {
+		Row row(segment.attributes.size());
+		const char* data = record.getData();
+
+		for (auto& attribute : segment.attributes) {
+			Register* r;
+
+			switch (attribute.type) {
+				case Type::Integer:
+					r = new Register(*reinterpret_cast<const Integer*>(data));
+					data += INTEGER_LEN;
+					break;
+
+				case Type::Char:
+					r = new Register(Char(data, CHAR_LEN));
+					data += CHAR_LEN;
+					break;
+
+				default:
+					assert(false);
+			}
+
+			row.push_back(r);
+		}
+
+		return row;
+	}
+
+}
---
+++ database/operator/TableScanOperator.h
@@ -0,0 +1,56 @@
+//
+//  TableScanOperator.h
+//  database
+//
+//  Created by Jan Michael Auer on 14/06/14.
+//  Copyright (c) 2014 LightningSQL. All rights reserved.
+//
+
+#pragma once
+
+#include "schema/Relation.h"
+#include "segment/SlottedPage.h"
+#include "segment/SPSegment.h"
+#include "IOperator.h"
+
+namespace lsql {
+
+	/**
+	 *
+	 */
+	class TableScanOperator : public IOperator {
+
+		Relation& segment;
+		SlottedPage* page;
+
+		SPSegment::Iterator pages;
+		SlottedPage::Iterator tuples;
+
+		bool isOpen;
+		Row output;
+
+	public:
+
+		/**
+		 *
+		 */
+		TableScanOperator(Relation& relation);
+
+		// IOperator interface implementation.
+
+		void open();
+		bool next();
+		Row getOutput() const;
+		void rewind();
+		void close();
+
+	private:
+
+		/**
+		 *
+		 */
+		Row buildRow(const Record& record) const;
+
+	};
+
+}
